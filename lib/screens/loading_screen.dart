932482f4faae005b0c8eef65cabd196f5f3e7b6c
Harsh <harshmacc@gmail.com>
2019-12-29 19:47:57 +0530

    used open weather api to grab weather details for the latitude and longitude

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 
 
@@ -19,6 +19,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.latitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+
+    if (response.statusCode == 200){
+      print(response.body);
+    }
+    else {
+      print(response.statusCode);
+    }
+  }
   @override
   void initState() {
     // TODO: implement initState

05728e081c68ee4d7d7b3581a4515e53a8d051f2
Harsh <harshmacc@gmail.com>
2019-12-29 19:27:43 +0530

    imported http library for network use

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
+
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,12 +12,11 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
 
   void getLocation() async {
-    try{Position position = await Geolocator()
-        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-        print(position);}
-        catch(e){
-          print(e);
-        }
+
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.longitude);
+    print(location.latitude);
   }
 
   @override
